b541f73589e94b6aca1e38dcbfdbc612dbb3a1cf
Xei-pher <seiferboado101@gmail.com>
2024-06-13 20:37:12 +0800

    initial login and signup frontend

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.port;
 
+app.use(express.static('public'));
 // Middleware
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', (req, res) => {
-    res.sendFile('templates/login.html');
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/signup', (req, res) => {
-    res.sendFile(__dirname + '/signup.html');
-});
-
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    // Check username and password in the database
-    // Example: db.collection('users').findOne({ username, password }, (err, user) => { ... });
-    // If user is found, authenticate
-    res.send(`Logged in as ${username}`);
-});
-
-app.post('/signup', (req, res) => {
-    const newUsername = req.body.newUsername;
-    const newPassword = req.body.newPassword;
-    // Save new user to the database
-    // Example: db.collection('users').insertOne({ username: newUsername, password: newPassword }, (err, result) => { ... });
-    res.send(`Signed up as ${newUsername}`);
-});
+app.use('/', require('./server/routes/main'));
 
 // Start server
 app.listen(port, () => {
