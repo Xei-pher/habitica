60c8789a37f432c44f52ba919a125103ca24ad96
Xei-pher <113429536+Xei-pher@users.noreply.github.com>
2024-07-25 20:07:42 +0800

    Add delete habit functionality

--- server/routes/habitica.js
+++ server/routes/habitica.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../middleware/isLoggedIn');
-
+const { ObjectId } = require('mongodb');
 // Get routes
 router.get('/home', isLoggedIn, (req, res) => {
     res.render('home', { fname: req.session.fname });
@@ -49,6 +49,24 @@ router.post('/createhabit', isLoggedIn, async (req, res) => {
         console.error('Error creating habit:', error);
         req.flash('error', 'An error occurred while creating the habit.');
         res.redirect('/habits');
+    }
+});
+
+router.post('/deletehabit/:id', async (req, res) => {
+    console.log("jacy");
+    try {
+        const habitId = req.params.id;
+        console.log(habitId);
+        const db = req.app.locals.db;
+        const result = await db.collection('habits').deleteOne({ _id: new ObjectId(habitId) });
+        console.log(result);
+        if (result) {
+            res.json({ success: true });
+        } else {
+            res.json({ success: false, error: 'Habit not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Server error' });
     }
 });
 module.exports = router;
